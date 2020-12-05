87fd0cc39465e859c14662ac07c4b8095b90b314
cep104 <candicelizabethpeters@gmail.com>
2020-12-05 11:44:10 -0600

    commit tree 333b3e8fd677731eaccfb6d0625156b35e1fcf7c

--- src/reducers/manageRestaurant.js
+++ src/reducers/manageRestaurant.js
@@ -1,34 +1,37 @@
 import cuid from 'cuid';
-export const cuidFn = cuid
+export const cuidFn = cuid;
 
 export default function manageRestaurants(state = {
-    restaurants = [],
-    reviews = []
+  restaurants: [],
+  reviews: [],
 }, action) {
+  switch (action.type) {
 
-    switch(action.type) {
-        case 'ADD_RESTAURANT':
-            const restaurant = {text: action.text, id: cuidFn()}
-            return {
-                ...state,
-                restaurants: [...state.restaurants, restaurant]
-            }
+    case 'ADD_RESTAURANT':
 
-            case 'DELETE_RESTAURANT':
-                const restaurants = state.restaurants.filter(restaurant => restaurant.id !== action.id)
-                return {...state, restaurants}
+      const restaurant = { text: action.text, id: cuidFn() };
+      return {
+        ...state,
+        restaurants: [ ...state.restaurants, restaurant]
+      }
 
-            case 'ADD_REVIEW':
-                const review = {text: action.review.text, restaurantId: action.review.restaurantId, id: cuidFn()}
-                return {...state, reviews: [...state.reviews, review]}
+    case 'DELETE_RESTAURANT':
+      const restaurants = state.restaurants.filter(restaurant => restaurant.id !== action.id);
+      return { ...state, restaurants}
 
-            case 'DELETE_REVIEW':
-                const reviews = state.reviews.filter(review => review.id !== action.id)
-                return {...state, reviews}
+    case 'ADD_REVIEW':
 
-                default:
-                    return state
+      const review = { text: action.review.text, restaurantId: action.review.restaurantId, id: cuidFn() };
+      return { ...state,
+        reviews: [...state.reviews, review]
+      }
 
+    case 'DELETE_REVIEW':
+      const reviews = state.reviews.filter(review => review.id !== action.id);
+      return {...state, reviews }
 
-    }
-}
+    default:
+      return state;
+
+  }
+};
